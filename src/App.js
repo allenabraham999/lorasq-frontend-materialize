cee0914d6fc8ec493f70c317cab058467e3901d0
Bharath5324 <bharath.s@ieee.org>
2021-06-29 12:33:21 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from 'react'
+import 'materialize-css/dist/css/materialize.min.css'
+import {Switch, HashRouter as Router, Route} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Fragment>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/">
+            <h1>Landing Page</h1>
+          </Route>
+          <Route exact path="/dashboard">
+            <h1>Dashboard Page</h1>
+          </Route>
+          <Route exact path="/profile">
+            <h1>Profile page</h1>
+          </Route>
+          <Route exact path="/map">
+            <h1>Map Page</h1>
+          </Route>
+          <Route>
+            <h1>404 not Found</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import { Link } from 'react-router-dom';
+
+export default function NavBar()  {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">Logo</Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/map">Map</Link></li>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  };
