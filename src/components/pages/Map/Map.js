15ee5ab85766ebc2815621ed333e3698145592b9
allenabraham999 <allenabraham999@gmail.com>
2021-08-12 16:57:26 +0530

    Added Center changing and other map functionalities.

--- src/components/pages/Map/Map.js
+++ src/components/pages/Map/Map.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import L from "leaflet";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./Map.css";
@@ -13,24 +13,24 @@ var myIcon = L.icon({
 });
 
 
-function deleter(props,index){
-  console.log("I have been deleted!"+index);
-  console.log("Data:"+HelpNeeded[index])
+function deleter(props, index) {
+  console.log("I have been deleted!" + index);
+  console.log("Data:" + HelpNeeded[index])
   console.log(HelpNeeded[index])
   delete HelpNeeded[index];
   console.log(HelpNeeded);
 }
 
-function MarkerMaker(data,index) {
-  const [saved,changeSaved] = useState("Help Needed!!");
+function MarkerMaker(data, index) {
+  const [saved, changeSaved] = useState("Help Needed!!");
   return (
     <Marker position={[data.lat, data.lng]} icon={myIcon}>
       <Popup className="popup">
         <div className="contents">
           <b>{saved}</b> <br /> Latitude: {data.lat} <br /> Longitude:{data.lng}.
           <br></br>
-          <button onClick={()=>saved==="Help Needed!!" ?changeSaved("Saved"):changeSaved("Help Needed!!")} className="the-button">Saved</button>
-          <button onClick={(props)=>deleter(props,index)} className="the-button">Remove</button>
+          <button onClick={() => saved === "Help Needed!!" ? changeSaved("Saved") : changeSaved("Help Needed!!")} className="the-button">Saved</button>
+          <button onClick={(props) => deleter(props, index)} className="the-button">Remove</button>
         </div>
       </Popup>
     </Marker>
@@ -38,15 +38,15 @@ function MarkerMaker(data,index) {
 }
 
 
-function inputChange(x,pos,setPosition,position){
+function inputChange(x, pos, setPosition, position) {
   console.log("Happening!");
   console.log(Number(x.target.value));
-  if(pos===0){
+  if (pos === 0) {
     let same = position[1];
-    setPosition([Number(x.target.value),same]);
-  }else{
+    setPosition([Number(x.target.value), same]);
+  } else {
     let same = position[0];
-    setPosition([same,Number(x.target.value)]);
+    setPosition([same, Number(x.target.value)]);
   }
 }
 
@@ -56,8 +56,8 @@ function Mapp() {
     lng: -0.09,
     zoom: 13,
   };
-  const [modalIsOpen,setIsOpen] = useState(false);
-  const [position,setPosition] = useState([state.lat, state.lng]);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [position, setPosition] = useState([state.lat, state.lng]);
 
   return (
     <div className="map-holder">
@@ -76,20 +76,26 @@ function Mapp() {
         {HelpNeeded.map(MarkerMaker)}
       </MapContainer>
       <div className="settings">
-        <Modal isOpen= {modalIsOpen}>
-          <h1>Sike!!!</h1>
-          <label>Lattitude</label>
-          <input onChange={(event)=>inputChange(event,0,setPosition,position)}></input>
-          <br></br>
-          <label>Longitude</label>
-          <input onChange={(event)=>inputChange(event,1,setPosition,position)}></input>
-          <br></br>
-          <button>Submit</button>
-          <br></br>
-          <br></br>
-          <button onClick={()=>setIsOpen(false)}>Close It!</button>
+        <Modal isOpen={modalIsOpen}>
+          <div className="modal-con">
+          <button id="close" className="close-button" onClick={() => setIsOpen(false)}><i class="fas fa-times"></i></button>
+            <h1>Change center</h1>
+            <label>Lattitude</label>
+            <input onChange={(event) => inputChange(event, 0, setPosition, position)}></input>
+            <br></br>
+            <label>Longitude</label>
+            <input onChange={(event) => inputChange(event, 1, setPosition, position)}></input>
+            <br></br>
+            <br></br>
+            <button>Submit</button>
+            {/* <br></br> */}
+            <br></br>
+            {console.log("position is being printed!")}
+            {console.log(position)}
+            {console.log("position has been printed!")}
+          </div>
         </Modal>
-        <button onClick={()=>setIsOpen(true)} className="button">
+        <button onClick={() => setIsOpen(true)} className="button">
           Settings
         </button>
       </div>
