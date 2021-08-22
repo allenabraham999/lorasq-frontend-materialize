aa875702f5bd577a55895cc99345607a0748b693
allenabraham999 <allenabraham999@gmail.com>
2021-08-23 03:04:28 +0530

    Added the profile Section

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,29 +1,21 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import { Card } from 'reactstrap';
 import x from "./Data";
 import "./Profile.css";
 import ReactModal from 'react-modal';
 import Modal from 'react-modal';
 import { func } from 'prop-types';
-import { preProcessFile } from 'typescript';
+import { convertToObject, preProcessFile } from 'typescript';
 
-function handleChange(key,newData,setNewData){
-  console.log("Here!");
-  console.location(key);
-  setNewData(prevValue=>{
-    console.log(prevValue);
-  }
-  );
-}
-
-function handleSubmit(newData){
-
+function handleSubmit(newData,x) {
+  x = newData;
+  console.log("The x data is:");
+  console.log(x)
 }
 
 function Profile() {
-  console.log(x);
-  const [modalIsOpen,setOpen] = useState(false);
-  const [newData,setNewData] = useState(x);
+  const [modalIsOpen, setOpen] = useState(false);
+  const [newData, setNewData] = useState(x);
   return (
     <div>
       <div className="profile-container">
@@ -52,42 +44,42 @@ function Profile() {
             </tr>
           </table>
         </div>
-        <button onClick={()=>{setOpen(true)}} className="edit-user-button"><i class="fas fa-user-edit"></i></button>
+        <button onClick={() => { setOpen(true) }} className="edit-user-button"><i class="fas fa-user-edit"></i></button>
       </div>
-    
-    <Modal isOpen= {modalIsOpen}>
-    <button id="close" className="close-button" onClick={() => setOpen(false)}><i class="fas fa-times"></i></button>
-      <div className="edit-container">
-        <div>
-        <label id = "name">Name</label>
-        <input type="text" for="name" onChange={()=>handleChange("name",newData,setNewData)}></input>
-        </div>
-        <div>
-        <label id = "email">Email</label>
-        <input type="email" for="email" onChange={handleChange("email",newData,setNewData)}></input>
-        </div>
-        <div>
-        <label id = "pno">Phone No.</label>
-        <input type="number" for="pno" onChange={handleChange("phoneno",newData,setNewData)}></input>
-        </div>
-        <div>
-        <label id = "loc">Location</label>
-        <input type="text" for="loc" onChange={handleChange("location",newData,setNewData)}></input>
-        </div>
-        <div>
-        <label id = "ppl">No. of People</label>
-        <input type="text" for="ppl" onChange={handleChange("nop",newData,setNewData)}></input>
-        </div>
-      </div>
-      <div className="button-wrapper">
-        <button className="submitter" onClick={handleSubmit(newData)}>
-          Submit
-        </button>
-      </div>
-    </Modal>
 
-
+      <Modal isOpen={modalIsOpen}>
+        <button id="close" className="close-button" onClick={() => setOpen(false)}><i class="fas fa-times"></i></button>
+        <div className="edit-container">
+          <div>
+            <label id="name">Name</label>
+            <input type="text" for="name" placeholder={x.name} onChange={(event) => { setNewData({ ...newData, "name": event.target.value }) }}></input>
+            <div />
+            <div>
+              <label id="email">Email</label>
+              <input type="email" for="email" placeholder={x.email} onChange={(event) => { setNewData({ ...newData, "email": event.target.value }) }}></input>
+            </div>
+            <div>
+              <label id="pno">Phone No.</label>
+              <input type="number" for="pno" placeholder={x.phoneno} onChange={(event) => { setNewData({ ...newData, "phoneno": event.target.value }) }}></input>
+            </div>
+            <div>
+              <label id="loc">Location</label>
+              <input type="text" for="loc" placeholder={x.location} onChange={(event) => { setNewData({ ...newData, "location": event.target.value }) }}></input>
+            </div>
+            <div>
+              <label id="ppl">No. of People</label>
+              <input type="text" for="ppl" placeholder={x.nop} onChange={(event) => { setNewData({ ...newData, "nop": event.target.value }) }}></input>
+            </div>
+          </div>
+          <div className="button-wrapper">
+            <button className="submitter" onClick={() => handleSubmit(newData,x)}>
+              Submit
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
+
   );
 }
 
